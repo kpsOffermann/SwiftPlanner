66f1bec6430364e87892c4efe89b1b4552ed8fc5
kpsOffermann <s.offermann@kpsbremen.de>
2023-02-11 13:22:39 +0100

    SelectionSorter

---
+++ Sources/SwiftPlanner/core/impl/heuristic/selector/common/decorator/SelectionSorter.swift
@@ -0,0 +1,51 @@
+//
+/*
+ Copied and adapted from OptaPlanner (https://github.com/kiegroup/optaplanner)
+
+ Copyright 2006-2023 kiegroup
+ Copyright 2023 KPS Software GmbH
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+
+ NOTICE: This file is based on the optaPlanner engine by kiegroup 
+         (https://github.com/kiegroup/optaplanner), which is licensed 
+         under the Apache License, Version 2.0, too. Furthermore, this
+         file has been modified including (but not necessarily
+         limited to) translating the original file to Swift.
+ */
+
+/**
+ * Decides the order of a {@link List} of selection
+ * (which is a {@link PlanningEntity}, a planningValue, a {@link Move} or a {@link Selector}).
+ *
+ * <p>
+ * Implementations are expected to be stateless.
+ * The solver may choose to reuse instances.
+ *
+ * @param <Solution_> the solution type, the class with the {@link PlanningSolution} annotation
+ * @param <T> the selection type
+ */
+public protocol SelectionSorter<Solution_, T> {
+    
+    associatedtype Solution_
+    associatedtype T
+    
+    /**
+     * @param scoreDirector never null, the {@link ScoreDirector}
+     *        which has the {@link ScoreDirector#getWorkingSolution()} to which the selections belong or apply to
+     * @param selectionList never null, a {@link List}
+     *        of {@link PlanningEntity}, planningValue, {@link Move} or {@link Selector}
+     */
+    func sort(scoreDirector: some ScoreDirector<Solution_>, selectionList: inout [T])
+
+}
